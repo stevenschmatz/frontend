9c395944fe7cfbeb48ae49412fc659680cb5b505
vinay427 <vhiremath95@gmail.com>
2015-08-12 23:08:40 -0400

    Added a basic gulp task that compiles the JSX files

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,33 +4,16 @@ var gulp = require('gulp');
 var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
 var browserify = require('browserify');
 var watchify = require('watchify');
-var reactify = require('reactify'); 
+var reactify = require('reactify');
+var react = require('gulp-react');
 var jade = require('gulp-jade');
- 
-gulp.task('browserify', function() {
-    var bundler = browserify({
-        entries: ['assets/js/main.js'], // Only need initial file, browserify finds the deps
-        transform: [reactify], // We want to convert JSX to normal javascript
-        debug: true, // Gives us sourcemapping
-        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
-    });
-    var watcher = watchify(bundler);
 
-    return watcher
-    .on('update', function () { // When any files update
-        var updateStart = Date.now();
-        console.log('Updating!');
-        watcher.bundle() // Create new bundle that uses the cache for high performance
-        .pipe(source('main.js'))
-    // This is where you add uglifying etc.
-        .pipe(gulp.dest('./build/'));
-        console.log('Updated!', (Date.now() - updateStart) + 'ms');
-    })
-    .bundle() // Create the initial bundle when starting the task
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('./build/'));
+gulp.task('react', function () {
+    return gulp.src('assets/js/**/*')
+        .pipe(react())
+        .pipe(gulp.dest('public/assets/js')); // in this line need dumanic destination
 });
-
+ 
 gulp.task('jade', function() {
   gulp.src('assets/jade/*.jade')
     .pipe(jade())
@@ -38,8 +21,6 @@ gulp.task('jade', function() {
 });
 
 gulp.task('assets', function() {
-  gulp.src('assets/js/**/*')
-    .pipe(gulp.dest('public/assets/js'));
   gulp.src('assets/css/**/*')
     .pipe(gulp.dest('public/assets/css'));
   gulp.src('assets/imgs/**/*')
@@ -49,4 +30,4 @@ gulp.task('assets', function() {
 
 });
 
-gulp.task('build', ['jade', 'assets']);
+gulp.task('build', ['react','jade', 'assets']);
