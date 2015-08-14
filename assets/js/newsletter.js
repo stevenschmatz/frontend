8973f085c4db905ab45b5870bd63e8b57835822c
vinay427 <vhiremath95@gmail.com>
2015-08-13 21:11:08 -0400

    Fixed site so home page works

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -1,37 +1,13 @@
-var NewsletterForm = React.createClass({
-    handleSubmit: function(e) {
-        e.preventDefault();
-        var name = React.findDOMNode(this.refs.name).value.trim();
-        var email = React.findDOMNode(this.refs.email).value.trim();
-        if (!name || !email) {
-            return;
-        }
-        $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            type: 'POST',
-            data: {
-                name: name,
-                email: email
-            },
-            error: function(xhr, status, err) {
-                console.error(this.props.url, status, err.toString());
-            }.bind(this)
-        });
-    },
-
-    render: function() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <input type="text" name="name" placeholder="Name" ref="name"/>
-                <input type="email" name="email" placeholder="Email" ref="email"/>
-                <input type="submit" value="Subscribe"/>
-            </form>
-        );
-    }
-})
-
-React.render(
-    <NewsletterForm url="https://api.optimizemi.org/newsletter"/>,
-    document.getElementById('contact')
-);
+$('#newsletter').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        url: 'https://api.optimizemi.org/newsletter',
+        dataType: 'json',
+        type: 'POST',
+        data: {
+            name: $('#newsletter-name').val(),
+            email: $('#newsletter-email').val()
+        },
+    });
+    return false;
+});
