e381154645883ee941ca89b521c10647bbacd198
john-dickinson <johndi@umich.edu>
2015-09-07 21:02:46 -0400

    Get involved button fade issue fixed [issue #12]

--- js/script.js
+++ js/script.js
@@ -24,6 +24,13 @@ $(document).ready(function() {
 	isVideoPlaying = false;
 	isPopupShowing = false;
 	isSocialVisible = false;
+	var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+    var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
+    var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+    var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+    var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+    if ((is_chrome)&&(is_safari)) {is_safari=false;}
+    if ((is_chrome)&&(is_opera)) {is_chrome=false;}
 	window = $('window');
 
 	var heroTextModule = {
@@ -187,6 +194,16 @@ $(document).ready(function() {
 		}
 	});
 
+	// $('.socialLogo').hover(function(){
+	// 	if(is_safari){
+	// 		alert('is safari');
+	// 		// $(this).removeClass('socialLogo',function(){
+	// 		// 	$(this).addClass('safariSocialLogo');
+	// 		// });
+	// 	} else {
+	// 		alert('is not safari');
+	// 	}
+	// })
 
 	$(window).scroll(function(event){
 
